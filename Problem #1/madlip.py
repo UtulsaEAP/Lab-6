e9997cb7f78f4d4ea4fa502fcb3a5433d4d8e6a8
DanteCastilleja <dsc2386@utulsa.edu>
2024-03-01 15:27:13 -0600

    test 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -2,8 +2,10 @@ def food_input():
     user_input = input()
     tokens = user_input.split()
     #type you while  loop here
-
-    
-
+    while True :
+        if 'quit' in user_input :
+            break
+        else :
+            return
 if __name__ == "__main__":
     food_input()
