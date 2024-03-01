b354726e93fe1603a8be7f08261fc8ed88372236
DanteCastilleja <dsc2386@utulsa.edu>
2024-03-01 15:50:32 -0600

    problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,13 @@
+"""
+Name: Dante Castilleja
+Lab Time: Friday 3:00
+"""
 def check_palindrome(user_input):
- #type your code here
+    pal = user_input [::-1]
+    if pal == user_input :
+        print(f'palindrome: {pal}')
+    else :
+        print(f'not a palindrome: {user_input}')
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
