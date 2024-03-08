d519906219ae8c4f7a1d469f33995ec6ccbf32d8
DanteCastilleja <dsc2386@utulsa.edu>
2024-03-07 21:46:58 -0600

    Problem 4

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,14 +1,16 @@
+"""
+Name: Dante Castilleja
+Lab Time: Friday 3:00
+"""
 def process_and_print(input_string):
       # Split into separate strings
-
+  input_string = list(map(int, user_input.split()))
     # Convert strings to integers and filter out negative values
-    input_data = 
-
+  input_data = [num for num in input_string if num < 0]
     # Sort integers in reverse order
-  
+  input_data.sort(reverse= True)
     # Print sorted integers
-    
-
+  print(*input_data)
 if __name__ == "__main__":
     # User inputs string w/ numbers
     user_input = input("Enter a space-separated string of numbers: ")
