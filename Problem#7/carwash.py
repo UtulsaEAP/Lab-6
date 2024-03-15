4539ad856d6d7541ef46dc1514e62d324ed2dabe
DanteCastilleja <dsc2386@utulsa.edu>
2024-03-15 15:24:38 -0500

    ----

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -8,11 +8,11 @@ def calculate_car_wash_price(service_choice1, service_choice2):
     total = 0
     service = []
     if service_choice1 == '-' and service_choice2 == '-' :
-        print('ZyCar Wash\nBase car wash - $10\n\nTotal price - $10')
+        print('ZyCar Wash\nBase car wash - $10\n-----\nTotal price - $10')
     elif service_choice2 == '-' :
-        print(f'ZyCar Wash\nBase car wash - $10\n{service_choice1} - ${services[service_choice1]}\n\nTotal price - ${base_wash + services[service_choice1]}')
+        print(f'ZyCar Wash\nBase car wash - $10\n{service_choice1} - ${services[service_choice1]}\n-----\nTotal price - ${base_wash + services[service_choice1]}')
     else :
-        print(f'Zycar Wash\nBase car wash - $10\n{service_choice1} - ${services[service_choice1]}\n{service_choice2} - ${services[service_choice2]}\n\nTotal price - ${base_wash + services[service_choice1] + services[service_choice2]}')
+        print(f'Zycar Wash\nBase car wash - $10\n{service_choice1} - ${services[service_choice1]}\n{service_choice2} - ${services[service_choice2]}\n-----\nTotal price - ${base_wash + services[service_choice1] + services[service_choice2]}')
 
     
 if __name__ == '__main__':
