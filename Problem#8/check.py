6c0528bcbbbe4ec978c17cf22ca1ecdcb73c4c42
DanteCastilleja <dsc2386@utulsa.edu>
2024-03-27 14:19:54 -0500

    Problem 8

--- Problem#8/check.py
+++ Problem#8/check.py
@@ -3,13 +3,16 @@ Name: Dante Castilleja
 Lab Time: Friday 3:00
 """
 def in_order(nums):
-    # Type your code here.
+   for num in range(len(nums) - 1) :
+       if nums[num] > nums[num + 1] :
+           return False
+   return True
 if __name__ == '__main__':
     # Test out-of-order example
     nums1 = [5, 6, 7, 8, 3]
     if in_order(nums1):
         print('In order')
-    else:
+    else: 
         print('Not in order')
         
     # Test in-order example
